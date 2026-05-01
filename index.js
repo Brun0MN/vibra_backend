3712233e527e5c421aa6c9b2c4bb9e9bb8f2321c
Bruno <brunomn9@hotmail.com>
2026-04-30 23:04:40 -0300

    ajusta tendencia para vibracao_resumo

--- index.js
+++ index.js
@@ -765,7 +765,7 @@ app.get("/tendencia_influx", async (req, res) => {
     const query = `
       from(bucket: "${process.env.INFLUX_BUCKET}")
         |> range(start: -24h)
-        |> filter(fn: (r) => r._measurement == "vibracao")
+        |> filter(fn: (r) => r._measurement == "vibracao_resumo")
         |> filter(fn: (r) => r.machineId == "${machineId}")
         |> filter(fn: (r) => r._field == "vrmsVelGlobal")
         |> sort(columns: ["_time"])
