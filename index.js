4845e9a6642430bd0c227a9f2a3521dc604b433e
Bruno <brunomn9@hotmail.com>
2026-05-06 19:55:19 -0300

    limita tendencias por numero de pontos e nao por data

--- index.js
+++ index.js
@@ -1105,6 +1105,7 @@ app.get("/tendencia_acel_influx", async (req, res) => {
         |> filter(fn: (r) => r.machineId == "${machineId}")
         |> filter(fn: (r) => r._field == "vrmsGlobal")
         |> sort(columns: ["_time"])
+        |> limit(20)
     `;
 
     const pontos = [];
