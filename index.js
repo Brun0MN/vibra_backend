2ac3760879a7041d1b278d90b3d8bbe9baa40fbe
Bruno <brunomn9@hotmail.com>
2026-05-06 20:45:24 -0300

    limita tendencias por numero de pontos e nao por data

--- index.js
+++ index.js
@@ -824,7 +824,7 @@ app.get("/tendencia_influx", async (req, res) => {
         |> range(start: -30d)
         |> filter(fn: (r) => r._measurement == "vibracao_resumo")
         |> filter(fn: (r) => r.machineId == "${machineId}")
-        |> filter(fn: (r) => r._field == "vrmsGlobal")
+        |> filter(fn: (r) => r._field == "vrmsVelGlobal")
         |> group()
         |> sort(columns: ["_time"])
         |> tail(n: 10)
